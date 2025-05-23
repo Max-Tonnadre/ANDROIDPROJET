74d5e7080e123e605fce049ddca6909d618f9b42
Max-Tonnadre <max.tonnadre@efrei.net>
2025-05-23 13:39:22 +0200

    ++

--- ProjectAndroid/app/build.gradle.kts
+++ ProjectAndroid/app/build.gradle.kts
@@ -47,5 +47,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("org.postgresql:postgresql:42.6.0")
+    //implementation("org.postgresql:postgresql:42.6.0")
 }
